4d444489596ef464b3a2a25c2ef2a7ddc625bee5
Alex Eisenhauer <eisea904@newschool.edu>
2020-07-15 16:18:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,43 @@
+require 'pry'
+
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  dogs = ["scooby", "shaggy", "fido", "lassie"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  binding.pry
+  arrays! = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
